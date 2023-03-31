9576fa0f55703e8fe10a1f364459765dc8458683
EigaCerion <yusufaristokrat@gmail.com>
2023-04-01 01:19:32 +0700

    sedikit penambahan pada Homeview

--- lib/view/pages/HomeView.dart
+++ lib/view/pages/HomeView.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 
@@ -10,51 +11,184 @@ class HomeView extends StatelessWidget {
     return Scaffold(
       body: SafeArea(
         child: SingleChildScrollView(
-          child: Container(
-            width: double.infinity,
-            height: 188.9,
-            decoration: BoxDecoration(
-              color: Color(0xFFD9D9D9),
-              borderRadius: BorderRadius.only(
-                bottomLeft: Radius.circular(30),
-                bottomRight: Radius.circular(30),
-              ),
-            ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                PopupMenuButton(
-                  offset: Offset(-30, 65), // mengatur posisi menu dropdown
-                  itemBuilder: (BuildContext context) => <PopupMenuEntry>[
-                    const PopupMenuItem(
-                      child: Text('Edit Profil'),
-                    ),
-                    const PopupMenuItem(
-                      child: Text('Log Out'),
-                    ),
-                  ],
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      SizedBox(
-                        width: 10.0,
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(20.0),
-                        child: CircleAvatar(
-                          radius: 20.0,
-                          backgroundImage:
-                              AssetImage('assets/images/logo.png'),
-                        ),
-                      ),
-                    ],
+          child: Stack(
+            children: [
+              Container(
+                width: double.infinity,
+                height: 188.9,
+                decoration: BoxDecoration(
+                  color: Color(0xFFD9D9D9),
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(30),
+                    bottomRight: Radius.circular(30),
                   ),
                 ),
-              ],
-            ),
+              ),
+              ContainerFill(),
+              const Profile(),
+              TopWidget(),
+            ],
           ),
         ),
       ),
+    );
+  }
+}
+
+class TopWidget extends StatelessWidget {
+  const TopWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        TextWidget(),
+      ],
+    );
+  }
+}
+
+class TextWidget extends StatelessWidget {
+  const TextWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 50, left: 20, right: 20),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Yusuf Aristokrat',
+                style: TextStyle(
+                    fontSize: 16,
+                    fontFamily: 'Poppins',
+                    fontWeight: FontWeight.bold),
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    '8937647829',
+                    style: TextStyle(
+                      fontSize: 12,
+                      fontFamily: 'Poppins',
+                      fontWeight: FontWeight.normal,
+                    ),
+                  ),
+                  Text(
+                    ' | ',
+                    style: TextStyle(fontSize: 12),
+                  ),
+                  Text(
+                    'Sistem Informasi',
+                    style: TextStyle(
+                      fontSize: 12,
+                      fontFamily: 'Poppins',
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ContainerFill extends StatelessWidget {
+  const ContainerFill({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned.fill(
+      child: ColorFiltered(
+        colorFilter: ColorFilter.mode(
+          Color(0xFFD9D9D9).withOpacity(0.8),
+          BlendMode.dstATop,
+        ),
+        child: Opacity(
+          opacity: 0.5,
+          child: Image.asset(
+            'assets/images/Learning.png',
+            fit: BoxFit.fitHeight,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Profile extends StatelessWidget {
+  const Profile({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        PopupMenuButton(
+          offset: Offset(-30, 123), // mengatur posisi menu dropdown
+          itemBuilder: (BuildContext context) => <PopupMenuEntry>[
+            PopupMenuItem(
+              child: Row(
+                children: [
+                  Icon(Icons.person),
+                  SizedBox(width: 5.0),
+                  Text(
+                    'Edit Profil',
+                    style: TextStyle(
+                      fontSize: 12,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            PopupMenuItem(
+              child: Row(
+                children: [
+                  Icon(Icons.logout),
+                  SizedBox(width: 5.0),
+                  Text(
+                    'Log Out',
+                    style: TextStyle(
+                      fontSize: 12,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10.0),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.only(
+              right: 30,
+              top: 45,
+              left: 0,
+              bottom: 0,
+            ),
+            child: CircleAvatar(
+              radius: 38.0,
+              backgroundImage: AssetImage('assets/images/logo.png'),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
